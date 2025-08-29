c1362d51b59356837b04c7b40cf76b75c8046942
pk-vishnu <vishnuvpk05@gmail.com>
2025-08-29 13:00:36 +0530

    feat: buffer management

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,13 @@
 vim.g.mapleader = " "
+local keymap = vim.keymap.set
 
-vim.keymap.set("n","<leader>e",":Ex<CR>")
+-- FILE OPS
+keymap("n", "<leader>e", ":Ex<CR>", { desc = "Open netrw" })
+
+-- BUFFERS
+-- Navigation
+keymap("n", "<S-l>", ":bnext<CR>", { desc = "Next buffer" })
+keymap("n", "<S-h>", ":bprevious<CR>", { desc = "Prev buffer" })
+keymap("n","<leader>bd", ":bd<CR>", { desc = "Delete buffer" })
+keymap("n", "<leader>bl", ":ls<CR>", { desc = "List buffers" })
+
