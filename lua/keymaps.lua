e135d711374c71d9cf73262f0f76c1fb4e3bf472
pk-vishnu <vishnuvpk05@gmail.com>
2025-08-29 17:46:04 +0530

    feat: buffer split mapppings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -14,4 +14,26 @@ keymap("n", "<leader>bl", ":ls<CR>", { desc = "List buffers" })
 keymap("n", "<leader>bf", ":b ", { desc = "Fuzzy find buffer" })
 keymap("n", "<leader>bda", ":%bd|e#<CR>", {desc = "Delete all buffers except current"})
 
+-- move between splits
+keymap('n', '<leader>h', '<C-w>h') -- left
+keymap('n', '<leader>j', '<C-w>j') -- down
+keymap('n', '<leader>k', '<C-w>k') -- up
+keymap('n', '<leader>l', '<C-w>l') -- right
 
+-- resize splits
+keymap('n', '<leader><', '<C-w><')  -- shrink width
+keymap('n', '<leader>>', '<C-w>>')  -- increase width
+keymap('n', '<leader>+', '<C-w>+')  -- increase height
+keymap('n', '<leader>-', '<C-w>-')  -- decrease height
+
+-- close splits
+keymap('n', '<leader>q', '<C-w>c')  -- close current split
+keymap('n', '<leader>o', '<C-w>o')  -- close all other splits
+
+-- open splits
+keymap('n', '<leader>sv', ':vsplit<CR>')  -- vertical split
+keymap('n', '<leader>sh', ':split<CR>')    -- horizontal split
+
+-- move buffer to a new split
+keymap('n', '<leader>vh', ':vsplit | wincmd l<CR>')  -- move right
+keymap('n', '<leader>vj', ':split | wincmd j<CR>')    -- move down
