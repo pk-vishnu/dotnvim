c731d9663173629655141d9071e28f24e467233c
pk-vishnu <vishnuvpk05@gmail.com>
2025-10-06 21:42:08 +0530

    feat: nerdfont icons, telescope layout modification

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,15 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     local keymap = vim.keymap.set
+    require("telescope").setup({
+      defaults = {
+        layout_config = {
+          height = 100,
+          width = 400,
+          prompt_position = "top",
+        },
+      },
+    })
     local builtin = require("telescope.builtin")
     keymap("n", "<C-p>", builtin.git_files, { desc = "Telescope find files" })
     keymap("n", "<leader>ff", builtin.find_files, { desc = "Telescope find git tracked files" })
