ad6ef326c9c0c50e819efd5d4c725e1fb7faf791
pk-vishnu <vishnuvpk05@gmail.com>
2025-11-17 23:04:41 +0530

    fix: remove mini statusline gitbranch

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,28 +3,6 @@ return {
     "echasnovski/mini.nvim",
     config = function()
       local statusline = require("mini.statusline")
-
-      local function git_branch()
-        local ok, head = pcall(function()
-          return vim.b.gitsigns_head
-        end)
-        if ok and head and head ~= "" then
-          return " " .. head
-        end
-        return ""
-      end
-
-      statusline.section_location = function()
-        local branch = git_branch()
-        local loc = "%l:%c"
-        if branch ~= "" then
-          return branch .. "  " .. loc
-        end
-        return loc
-      end
-
-      statusline.setup({ use_icons = true })
-
       statusline.setup({ use_icons = true })
     end,
   },
